f29291b034a9d9dc0932a18cfe1dd08b07af0ad1
GuilhermeDeconto <deconto100@gmail.com>
2020-11-09 21:05:57 -0300

    styles

--- dtool/src/components/BasicListStyle.tsx
+++ dtool/src/components/BasicListStyle.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+import colors from "../utils/colors";
+import sizes from "../utils/sizes";
 
 export const container = styled(View)`
 alignItems: "center",
@@ -15,9 +17,28 @@ right: 10,
 `
 
 export const textItem = styled(Text)`
-color: colors.text.primary,
+color: ${colors.text.primary},
 flexDirection: "row",
-fontSize: sizes.buttonText.main,
+fontSize: ${sizes.buttonText.main},
+fontStyle: "normal",
+fontWeight: "normal",
+padding: 16,
+`
+
+export const textPatient = styled(Text)`
+color: ${colors.text.primary},
+flexDirection: "row",
+fontSize: ${sizes.buttonText.main},
+fontStyle: "normal",
+fontWeight: "normal",
+padding: 16,
+fontWeight: "bold",
+`
+
+export const textPatientSubtitle = styled(Text)`
+color: ${colors.text.tertiary},
+fontSize: ${sizes.buttonText.label},
+flexDirection: "row",
 fontStyle: "normal",
 fontWeight: "normal",
 padding: 16,
