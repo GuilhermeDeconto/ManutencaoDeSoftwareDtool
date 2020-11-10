34751a1112dadc46360b9990a4e105ddb067eebe
GuilhermeDeconto <deconto100@gmail.com>
2020-11-09 21:18:12 -0300

    styles

--- dtool/src/components/BasicListStyle.tsx
+++ dtool/src/components/BasicListStyle.tsx
@@ -5,41 +5,41 @@ import colors from "../utils/colors";
 import sizes from "../utils/sizes";
 
 export const container = styled(View)`
-alignItems: "center",
-flexDirection: "row",
+alignItems: center,
+flexDirection: row,
 paddingBottom: 200,
-width: "100%",
+width: 100%,
 `
 
 export const iconButton = styled(TouchableOpacity)`
-position: "absolute",
+position: absolute,
 right: 10,
 `
 
 export const textItem = styled(Text)`
 color: ${colors.text.primary},
-flexDirection: "row",
+flexDirection: row,
 fontSize: ${sizes.buttonText.main},
-fontStyle: "normal",
-fontWeight: "normal",
+fontStyle: normal,
+fontWeight: normal,
 padding: 16,
 `
 
 export const textPatient = styled(Text)`
 color: ${colors.text.primary},
-flexDirection: "row",
+flexDirection: row,
 fontSize: ${sizes.buttonText.main},
-fontStyle: "normal",
-fontWeight: "normal",
+fontStyle: normal,
+fontWeight: normal,
 padding: 16,
-fontWeight: "bold",
+fontWeight: bold,
 `
 
 export const textPatientSubtitle = styled(Text)`
 color: ${colors.text.tertiary},
 fontSize: ${sizes.buttonText.label},
-flexDirection: "row",
-fontStyle: "normal",
-fontWeight: "normal",
+flexDirection: row,
+fontStyle: normal,
+fontWeight: normal,
 padding: 16,
 `
